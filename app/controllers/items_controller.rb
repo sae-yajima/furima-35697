b948c0789ad39384b73f529923fbafff1eff7eaa
sae-yajima <y.sae1338@icloud.com>
2021-05-31 19:22:04 +0900

    ログイン状態の有無で商品出品ページへ遷移する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,17 @@
 class ItemsController < ApplicationController
+    before_action :authenticate_user!,only:[:new,:create]
     def  index
-     
-        
+        # query = "SELECT * FROM items"
+        # @items = Item.find_by_sql(query)  
     end
 
-    
+    # def new
+    #     @item = Item.new
+        
+    # end
+
+    # def create
+    #     @item = Item.new
+    # end
    
    end
