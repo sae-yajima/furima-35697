3eb853de620948de6b53a71431cdc469eadbd56d
sae-yajima <y.sae1338@icloud.com>
2021-06-05 14:02:58 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
     before_action :authenticate_user!,only:[:new,:create]
-
+    before_action :move_to_index, except: [:index, :show ]
     
     def  index
         @items = Item.all.order(created_at:"desc")
@@ -23,13 +23,36 @@ class ItemsController < ApplicationController
     end
 
 
-
-
-
-
     def show
         @item = Item.find(params[:id])
       end
+
+
+      def edit
+        @item = Item.find(params[:id])
+        unless @item.user_id == current_user.id
+               redirect_to root_path
+              
+          end
+        
+      end
+
+
+      def update
+        @item = Item.find(params[:id])
+        @item.update(item_params)
+        if @item.save
+            redirect_to item_path(params[:id])
+       
+        end
+      end
+
+
+
+
+
+
+
     
 
    
@@ -42,7 +65,12 @@ def item_params
     params.require(:item).permit(:image,:item_name,:item_info,:category_id,:sales_status_id,:fee_status_id,:prefecture_id,:scheduled_delivery_id,:item_price).merge(user_id: current_user.id)
 end
 
-
+def move_to_index
+    redirect_to user_session_path
+    unless user_signed_in?
+    end
+     
+ end
 
 
 
