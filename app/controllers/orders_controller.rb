7511735f1c259c5ea11e974905cfebbe1dba4dc8
sae-yajima <y.sae1338@icloud.com>
2021-06-08 18:04:17 +0900

    購入情報をテーブルに保存する

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,19 +2,18 @@ class OrdersController < ApplicationController
     before_action :authenticate_user!, except: :index
     def index
         @item = Item.find(params[:item_id])
-    end
-
-    def new
         @order_address = OrderAddress.new
     end
-  
+
+    
     def create
-       @order_address = Order.new(order_params)
+       @item = Item.find(params[:item_id])
+       @order_address = OrderAddress.new(order_params)
        if @order_address.valid?
          @order_address.save
-        redirect_to rppt_path
+        redirect_to root_path
        else
-        render :new
+        render :index
        end
     end
 
@@ -25,6 +24,6 @@ class OrdersController < ApplicationController
 
     private
     def order_params
-        params.require(:order_address).permit(:postal_code, :prefecture, :city,:address,:phone_number,:building_name).merge(user_id: current_user.id)
+        params.require(:order_address).permit(:postal_code, :prefecture_id, :city,:address,:phone_number,:building_name).merge(user_id: current_user.id)
     end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,15 +1,15 @@
 class OrderAddress
     include ActiveModel::Model
-    attr_accessor :postal_code, :prefecture, :city,:address,:building_name,:phone_number, :user_id
+    attr_accessor :postal_code, :prefecture_id, :city,:address,:building_name,:phone_number, :user_id
 
     with_options presence: true do
-      validates :postal_code, :prefecture,
-      validates :city,:address,:phone_number,
+      validates :postal_code, :prefecture_id
+      validates :city,:address,:phone_number
       validates :user_id
     end
 
     def save
       order = Order.create(user_id: user_id)
-      Address.create(postal_code: postal_code, prefecture: prefecture, city: city, address: address, phone_number: phone_number, building_name: building_name, order_id: order.id)
+      Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, phone_number: phone_number, building_name: building_name, order_id: order.id)
     end
 end
