331f6056c132f8fa3e4fe12a57b2380b3956449d
sae-yajima <y.sae1338@icloud.com>
2021-06-11 16:20:18 +0900

    出品者がurlを直接入力して購入ページへ遷移できないようにする

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,16 @@
 class OrdersController < ApplicationController
-    before_action :authenticate_user!, only:[:index,:show]
-   
+    before_action :authenticate_user!, only:[:index,:create]
+    before_action :set_item, only: [:index,:create]
+    before_action :move_to_index, only:[:index,:create]
     def index
         @order_address = OrderAddress.new
-        @item = Item.find(params[:item_id])
+        
        
     end
 
     
     def create
-       @item = Item.find(params[:item_id])
+      
        @order_address = OrderAddress.new(order_params)
     
        if @order_address.valid?
@@ -39,7 +40,15 @@ class OrdersController < ApplicationController
             currency: 'jpy'       
         )
     end
-    
+    def move_to_index
+   
+        if  @item.user_id == current_user.id || @item.order.present?
+             redirect_to root_path
+         end   
+    end
+    def set_item
+        @item = Item.find(params[:item_id])
+    end  
 
 
 end
