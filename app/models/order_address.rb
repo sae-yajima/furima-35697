79cf5962831d8b4d16e3ad3ec53a5def3993026b
sae-yajima <y.sae1338@icloud.com>
2021-06-11 18:02:05 +0900

    商品購入機能の修正2

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,7 +1,6 @@
 class OrderAddress
     include ActiveModel::Model
-    attr_accessor :token
-    attr_accessor :postal_code, :prefecture_id, :city,:address,:building,:phone_number, :user_id,:item_id
+    attr_accessor :postal_code, :prefecture_id, :city,:address,:building,:phone_number, :user_id,:item_id,:token
 
     with_options presence: true do
       validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
